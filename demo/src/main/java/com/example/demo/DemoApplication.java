888426a9174a92fe3d063c4f2f3cf50810630b32
Andréas Cottet <andrecot2502@gmail.com>
2024-04-03 08:28:48 +0200

    Exemple : getting data by an api

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,30 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
+
     }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/test")
+    public String test(@RequestParam(value = "name", defaultValue = "World") String name) {
+      RestTemplate restTemplate = new RestTemplate();
+      String apiUrl = "https://opensky-network.org/api/states/all";
+      return  restTemplate.getForObject(apiUrl, String.class);
     }
 }
